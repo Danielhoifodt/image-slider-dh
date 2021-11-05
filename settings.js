70d8c9bf944f4da42479b4d4546c59a433fef581
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-05 23:52:08 +0100

    Setup Parcel and Prettier

--- settings.js
+++ settings.js
@@ -1,10 +1,9 @@
-const settings =
-{
-    duration:5000,
-    fadespeed:50,
-    arrows: true,
-    width:"1000px",
-    height:"500px"
-}
+const settings = {
+  duration: 5000,
+  fadespeed: 50,
+  arrows: true,
+  width: "1000px",
+  height: "500px",
+};
 
-export default settings
+export default settings;
