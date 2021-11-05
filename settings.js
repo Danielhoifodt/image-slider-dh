79b68bb2a9030ddfb42927117a6b0f745fa1df51
dh_netsense.no <dh@netsense.no>
2021-11-05 16:40:04 +0100

    arrows

--- settings.js
+++ settings.js
@@ -2,6 +2,7 @@ const settings =
 {
     duration:5000,
     fadespeed:50,
+    arrows: true,
     width:"1000px",
     height:"500px"
 }
--- slider.js
+++ slider.js
@@ -15,7 +15,46 @@ function load_img(image)
 
     img.setAttribute("src", image)
 
+    img.classList.add(".slider")
+
     fadeIn()
+}
+
+function next_prev()
+{
+    img.setAttribute("width", settings.width)
+
+    img.setAttribute("height", settings.height)
+
+    img.setAttribute("src", images[0])
+
+    img.classList.add("slider")
+
+    let arrow1 = document.createElement("div")
+    let arrow2 = document.createElement("div")
+
+    arrow1.classList.add("arrow1")
+    arrow2.classList.add("arrow2")
+
+    arrow1.addEventListener("click", next)
+    arrow2.addEventListener("click", prev)
+
+    img.appendChild(arrow1)
+    img.appendChild(arrow2)
+}
+
+function next()
+{
+    
+    let image = images ++
+    img.setAttribute("src", image)
+}
+
+function prev()
+{
+    
+    let image = images --
+    img.setAttribute("src", image)
 }
 
 function fadeIn ()
@@ -47,5 +86,10 @@ function timer ()
         }, settings.duration*i)
     }
 }
-
-timer()
+if(settings.arrows === true)
+{
+    next_prev()
+}
+else{
+    timer()
+}
