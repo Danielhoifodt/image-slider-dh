3a38bf88e45ad71beb33234999b0256f7f7d83f6
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-11 03:43:49 +0100

    Replace SCSS animations with Animate.css

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,10 +1,12 @@
 const settings = {
-    duration: 5000,
-    fadespeed: 2000,
+    duration: '3.0s',
+    fadespeed: 3000,
     // Set to false to disable arrows
     arrows: true,
     width: '1000px',
-    height: '500px'
+    height: '500px',
+    // Copy any animate in classname from https://animate.style
+    animateEffect: 'animate__rotateIn'
 };
 
 export default settings;
