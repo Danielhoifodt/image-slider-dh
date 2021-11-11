4a4314eb3252d6c865fd852f47f7c3fc03bf0724
Daniel Høifødt <40982934+Danielhoifodt@users.noreply.github.com>
2021-11-11 12:30:41 +0100

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,7 +2,7 @@ const settings = {
     // Animations
     duration: '3.0s',
     fadespeed: 3000,
-    animateEffect: 'animate__rotateIn', // Copy any animate in classname from https://animate.style
+    animateEffect: 'animate__zoomIn', // Copy any animate in classname from https://animate.style
     // UI
     arrows: true, // Set to false to disable arrows
     width: '1000px',
