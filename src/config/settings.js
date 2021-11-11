32ec236ee7917b5fceab3b50dbe4fcfe538866da
Daniel Fjeldstad <daniel.fjeldstad@novacare.no>
2021-11-11 04:05:13 +0100

    Improve settings explanation

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,12 +1,12 @@
 const settings = {
+    // Animations
     duration: '3.0s',
     fadespeed: 3000,
-    // Set to false to disable arrows
-    arrows: true,
+    animateEffect: 'animate__rotateIn', // Copy any animate in classname from https://animate.style
+    // UI
+    arrows: true, // Set to false to disable arrows
     width: '1000px',
-    height: '500px',
-    // Copy any animate in classname from https://animate.style
-    animateEffect: 'animate__rotateIn'
+    height: '500px'
 };
 
 export default settings;
